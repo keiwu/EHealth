cf0aad9e29dd71f76b995b81831769261f1a72cc
kei <keiwus@gmail.com>
2018-12-22 18:30:29 -0500

    Refactor the code to use MVP design pattern

---
+++ app/src/main/java/Presenter/SearchPresenter.java
@@ -0,0 +1,48 @@
+package Presenter;
+
+import com.su.kei.ehealthrecordsretrieval.MainActivity;
+
+import java.util.ArrayList;
+import Util.Utils;
+import bean.Patients;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import service.EHealthApi;
+import service.RetrofitHelper;
+import static constants.Constant.RECORDS_TO_RETRIEVE;
+
+public class SearchPresenter {
+    private SearchView searchView;
+
+    public SearchPresenter(MainActivity mainActivity) {
+        this.searchView = mainActivity;
+    }
+
+    public void getPatients() {
+        EHealthApi eHealthApi = RetrofitHelper.getRetrofit().create(EHealthApi.class);
+
+        Call<Patients> call = eHealthApi.getPatients(RECORDS_TO_RETRIEVE);
+
+        call.enqueue(new Callback<Patients>() {
+                         @Override
+                         public void onResponse(Call<Patients> call, Response<Patients> response) {
+                             ArrayList<Patients.EPatient> patientAl;
+                             patientAl = response.body().getEntry();
+                             patientAl = Utils.sortPatientList(patientAl);
+                             searchView.onItemRetrieved(patientAl);
+                         }
+
+                         @Override
+                         public void onFailure(Call<Patients> call, Throwable t) {
+                             searchView.onItemRetrieved(null);
+
+                         }
+                     }
+        );
+    }
+
+    public interface SearchView {
+       void onItemRetrieved(ArrayList<Patients.EPatient> patientsAl);
+    }
+}
--- app/src/main/java/com/su/kei/ehealthrecordsretrieval/MainActivity.java
+++ app/src/main/java/com/su/kei/ehealthrecordsretrieval/MainActivity.java
@@ -6,24 +6,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
-import Util.Utils;
+import Presenter.SearchPresenter;
 import adapter.PatientAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import bean.Patients;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import service.EHealthApi;
-import service.RetrofitHelper;
-import static constants.Constant.RECORDS_TO_RETRIEVE;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements SearchPresenter.SearchView {
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
     private ProgressBar progressBar;
@@ -38,36 +30,23 @@ public class MainActivity extends AppCompatActivity{
         failToRetrieveTv = findViewById(R.id.failToRetrieveTv);
         linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        getPatients();
+        SearchPresenter searchPresenter = new SearchPresenter(this);
+        searchPresenter.getPatients();
     }
 
-    public void getPatients() {
-        EHealthApi eHealthApi = RetrofitHelper.getRetrofit().create(EHealthApi.class);
-
-        Call<Patients> call = eHealthApi.getPatients(RECORDS_TO_RETRIEVE);
-
-        call.enqueue(new Callback<Patients>() {
-                         @Override
-                         public void onResponse(Call<Patients> call, Response<Patients> response) {
-                             progressBar.setVisibility(View.INVISIBLE);
-                             ArrayList<Patients.EPatient> patientAl;
-                             patientAl = response.body().getEntry();
-                             patientAl = Utils.sortPatientList(patientAl);
-                             PatientAdapter adapter = new PatientAdapter(getApplicationContext(), patientAl);
-                             recyclerView.setAdapter(adapter);
-                             recyclerView.setLayoutManager(linearLayoutManager);
-                             DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
-                                     linearLayoutManager.getOrientation());
-                             recyclerView.addItemDecoration(dividerItemDecoration);
-                         }
-
-                         @Override
-                         public void onFailure(Call<Patients> call, Throwable t) {
-                             progressBar.setVisibility(View.INVISIBLE);
-                             failToRetrieveTv.setVisibility(View.VISIBLE);
-                             Toast.makeText(getApplicationContext(), getString(R.string.fail_to_retrieve_records), Toast.LENGTH_LONG).show();
-                         }
-                     }
-        );
+    @Override
+    public void onItemRetrieved(ArrayList<Patients.EPatient> patientAl) {
+        progressBar.setVisibility(View.INVISIBLE);
+        if (patientAl!=null){
+            PatientAdapter adapter = new PatientAdapter(getApplicationContext(), patientAl);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                    linearLayoutManager.getOrientation());
+            recyclerView.addItemDecoration(dividerItemDecoration);
+        } else{
+            failToRetrieveTv.setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(), getString(R.string.fail_to_retrieve_records), Toast.LENGTH_LONG).show();
+        }
     }
 }
